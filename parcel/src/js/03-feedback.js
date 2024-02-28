1122045c57e3a7700252126ff6e01bd229263565
danuux <danut_laurentiu98@yahoo.com>
2024-02-28 19:56:15 +0200

    Update exercise 3

--- parcel/src/js/03-feedback.js
+++ parcel/src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message');
+
+emailInput.addEventListener('input', throttle(updateFormState, 500));
+messageInput.addEventListener('input', throttle(updateFormState, 500));
+form.addEventListener('submit', handleFormSubmit);
+
+const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+emailInput.value = savedState.email || '';
+messageInput.value = savedState.message || '';
+
+function updateFormState() {
+  localStorage.setItem('feedback-form-state', JSON.stringify({ email: emailInput.value, message: messageInput.value }));
+};
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const email = emailInput.value;
+  const message = messageInput.value;
+  
+  if (!email) {
+    return;
+    };
+
+  console.log({ email, message });
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
